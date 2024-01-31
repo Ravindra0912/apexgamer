1e4c1559fc00bd391a7ef4b517ae4a649a79c319
Ravindra Shrivastava <ravindra.shrivastava.9@gmail.com>
2024-02-01 01:00:57 +0530

    Initial commit

---
+++ service/rawgApiClient.js
@@ -0,0 +1,13 @@
+const axios = require('axios')
+
+const getRawgData = (path, params = {}) =>
+  axios({
+    method: "get",
+    url: `https://api.rawg.io/api/${path}`,
+    params: {
+      key: process.env.RAWG_API_KEY,
+      ...params,
+    },
+  });
+
+module.exports = { getRawgData };
